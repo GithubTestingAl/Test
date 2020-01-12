ce5b72bc574393e2796722bcd3983bf6aaf7e828
NickRyanSteer <36924254+NickRyanSteer@users.noreply.github.com>
2020-01-12 14:16:20 +0000

    First Run of Program
    This was the first run of writing a flie

--- Test.py
+++ Test.py
@@ -1,2 +1,10 @@
 # This is a test program
 # read in a number and calculate squares and cubes up to that number
+
+
+max_value = int(input("\nWhat is the max value, 0-100?  "))
+
+with open(f"OutputFile.txt", "w") as fo:
+    fo.write( "Hello \n")
+
+fo.close()
